6c1de1abfd1290f47d7112a250454e3b1bb9ee4f
Jason Young <jyoung1756@gmail.com>
2016-04-14 18:51:49 +0100

    Fixed minor issues with tsc and bundle

--- build/tasks/build/tsc.js
+++ build/tasks/build/tsc.js
@@ -9,7 +9,7 @@ function tsc(config) {
 		}
 		
 		// execute tsc and pipe output
-		var p = exec(command, config.watch ? cb : () => {});
+		var p = exec(command, config.watch ? () => {} : cb);
 		p.stdout.pipe(process.stdout);
 		p.stderr.pipe(process.stderr);
 		if (config.watch) {
